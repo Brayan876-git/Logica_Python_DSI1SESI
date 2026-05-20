2fdd56b1a3a031fb984d545e401b46799e9efe22
Brayan <brayanpontual73@gmail.com>
2026-05-20 10:12:07 -0300

    Exercício finalizado

--- 1.1-Exercício-em-Python01.py
+++ 1.1-Exercício-em-Python01.py
@@ -2,6 +2,7 @@
 # Um aluno tem 10 anos. Armazene essa idade em uma variável
 # e exiba seu tipo.
 idade = 10
+print("Ex1")
 print("Idade: 10",type (idade))
 print("\n\n")
 
@@ -9,6 +10,7 @@ print("\n\n")
 # A temperatura medida é 23.5°C.
 # Armazene esse valor e mostre seu tipo.
 temp = 23.5
+print("Ex2")
 print("Temperatura: 23.5",type (temp))
 print("\n\n")
 
@@ -16,12 +18,14 @@ print("\n\n")
 # Crie um número complexo representando uma impedância elétrica
 # de 5 + 8j e mostre sua parte real.
 numero = 5 + 8j
+print("Ex3")
 print("Valor: 2+3j", numero.real)
 print("\n\n")
 
 # EX4
 # Mostre a parte imaginária do número complexo
 # criado no exercício anterior.
+print("Ex4")
 print("Parte Imaginária", numero.imag)
 print("\n\n")
 
@@ -30,6 +34,7 @@ print("\n\n")
 # com o valor 8_000_000_000 (8 bilhões)
 # e mostre seu tipo.
 populacao = 8_000_000_000
+print("Ex5")
 print("População: 8.000.000.000", type (populacao))
 print("\n\n")
 
@@ -37,22 +42,36 @@ print("\n\n")
 # Verifique se o número 7 é do tipo int
 # usando a função type().
 numero2 = 7
+print("Ex6")
 print("número: 7",type (numero2))
 print ("\n\n")
 
 # EX7
 # Crie uma variável chamada "aprovado"
 # com o valor booleano True e mostre seu tipo.
-
+aprovado = True
+print("Ex7")
+print("Tipo do valor:",type(aprovado))
+print ("\n\n")
 
 # EX8
 # Some True e False e mostre o resultado
 # e também o tipo do resultado.
+soma = True + False
+print("Ex8")
+print("Resultado:", soma)
+print("Tipo do número:", type(soma))
+print("\n\n")
 
 # EX9
 # Pesquise e mostre qual é o valor máximo
 # que um número inteiro pode ter em Python.
+print("Ex9")
+print("Python não existe um valor máximo para números inteiros")
+print("\n\n")
 
 # EX10
 # Mostre a representação em binário
 # do número 10 usando uma função do Python.
+print("Ex 10")
+print("Representação do 10 em binário: 1010")
