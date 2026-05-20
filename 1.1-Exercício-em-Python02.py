185162225c03624256d8047acb4ce86424b21ae7
Brayan <brayanpontual73@gmail.com>
2026-05-20 14:40:34 -0300

    Editando 3

--- 1.1-Exercício-em-Python02.py
+++ 1.1-Exercício-em-Python02.py
@@ -13,7 +13,7 @@ print("\n\n")
 # Converta o número de ponto flutuante 7.999
 # para inteiro e observe o resultado.
 print("Ex2")
-float = int (7.999)
+numero = int (7.999)
 print("número(7.999)", float)
 print("Convertendo...", type(float))
 print("-------------------------------")
@@ -22,19 +22,19 @@ print("\n\n")
 # EX3
 # Converta a string "-3.14" para float
 # e multiplique o resultado por 2.
-valor1 ="-3.14"
+valor2 ="-3.14"
 print("Ex3")
-print(type (valor1))
+print(type (valor2))
 print("Multiplicando por 2:", -3.14 * 2)
 print("-------------------------------")
 print("\n\n")
+
 # EX4
 # Tente converter a string "cento e vinte"
 # para inteiro e observe o que acontece.
 valor2 = ("cento e vinte")
 print("Ex4")
-print(valor2,type (valor2))
-if (valor2 int)
+print(type (valor2))
 
 # EX5
 # Converta o número 42 para string
