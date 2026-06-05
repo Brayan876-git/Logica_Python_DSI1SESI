1f6d7f4e5c3ed19ef8f2d6a7cba33072b7954604
Brayan <brayanpontual73@gmail.com>
2026-06-05 16:13:14 -0300

    Treinamento2

--- Treinamento_em_Python/Treino.py
+++ Treinamento_em_Python/Treino.py
@@ -3,18 +3,76 @@ print("======= TREINAMENTO EM PYTHON ======")
 print("\n\n")
 
 
+#Numero inteiro em python
+
 numero_int = 10
 print("Valor:", numero_int)
 print("Tipo:", type (numero_int))
 print("--------------------------------------------------")
 
 
+#Numero decimal em python "float"
+
 numero_deci = 3.14
 print("Valor:", numero_deci)
 print("Tipo:", type(numero_deci))
 print("--------------------------------------------------")
 
+
+ #Numero complexo em python "j"
+
 numero_comp = 2 +3j
 print("Valor:", numero_comp)
 print("Tipo:", type(numero_comp))
 print("--------------------------------------------------")
+
+
+#Acessando cada parte do numero
+print("Parte real:", numero_comp.real)
+print("--------------------------------------------------")
+
+
+print("Parte imaginario:", numero_comp.imag)
+print("--------------------------------------------------")
+
+
+print("============== Conversões ================")
+
+# float -> int
+
+valor = int(3.9)
+print("int(3.9):", valor)
+print("Tipo:", type(valor))
+print("--------------------------------------------------")
+
+#strings -> int
+
+valor1 = "10"
+print(type(valor1))
+
+valor2 = int("10")
+print('int("10"):', valor2)
+print("Tipo:", type (valor2))
+print("--------------------------------------------------")
+
+#int -> float
+
+valor3 = float(10)
+print("float(10):", valor3)
+print("Tipo:", type (valor3))
+print("--------------------------------------------------")
+
+
+
+print("\n\n")
+print("=============== Treinamento de Strings ===============")
+print("\n\n")
+
+#Criação de strings
+
+texto1 = "Salve"
+texto2 = 'Gamers'
+texto3 = "Coxinha 'com catupiry'"
+texto4 = 'Coxinha "com catupiry"'
+
+print(texto1, texto2, texto3, texto4)
