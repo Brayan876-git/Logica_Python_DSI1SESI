e41f867af39242ee54e56b74881ad5987da048bb
Brayan <brayanpontual73@gmail.com>
2026-06-05 15:49:02 -0300

    Treinamento

---
+++ Treinamento_em_Python/Treino.py
@@ -0,0 +1,20 @@
+print("--------------------------------------------------")
+print("======= TREINAMENTO EM PYTHON ======")
+print("\n\n")
+
+
+numero_int = 10
+print("Valor:", numero_int)
+print("Tipo:", type (numero_int))
+print("--------------------------------------------------")
+
+
+numero_deci = 3.14
+print("Valor:", numero_deci)
+print("Tipo:", type(numero_deci))
+print("--------------------------------------------------")
+
+numero_comp = 2 +3j
+print("Valor:", numero_comp)
+print("Tipo:", type(numero_comp))
+print("--------------------------------------------------")
